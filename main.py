374329df4f83788de97cc61b165c7660f6b0ddec
Kendall <111667017+srgevrg@users.noreply.github.com>
2022-12-02 11:30:27 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#Monster List
-monster = ['Ratman', 'Goblin', 'Orc', 'Hobgoblin', 'Troll']
-monster_type = ['Cave', 'Swamp', 'Forest', 'Desert', 'Snow']
-monster_rank = ['Scout', 'Bandit', 'Warrior', 'Chief', 'Lord']
-monster_size = ['Puny', 'Smallish', 'Average-sized', 'Beefy', 'Huge']
-
-#Weapon List
-weapon = ['stick', 'dagger', 'sword', 'axe', 'hammer', 'maul']
-weapon_mat = ['wooden', 'iron', 'steel', 'mithril', 'crystal']
